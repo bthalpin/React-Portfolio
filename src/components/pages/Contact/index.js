b2b02372ace0b5f0aa1f30960fb2a0108d8675c8
Brian Halpin <brian.t.halpin@gmail.com>
2022-04-19 15:17:17 -0400

    Add errors for contact form

--- src/components/pages/Contact/index.js
+++ src/components/pages/Contact/index.js
@@ -5,9 +5,9 @@ function Contact () {
     const [name,SetName] = useState('')
     const [email,SetEmail] = useState('')
     const [message,SetMessage] = useState('')
-
+    const [errorMessage,setErrorMessage] = useState('')
     const handleInput = (e) => {
-        console.log(e)
+        
         const {name,value} = e.target
         if (name === 'name'){
             SetName(value)
@@ -19,9 +19,35 @@ function Contact () {
             SetMessage(value)
         }
     }
+    const checkInput = (e) => {
+        const {name,value} = e.target
+        
+        if (name === 'name'){
+            if (value===''){
+                setErrorMessage('Name is required')
+            }
+        }
+        if (name === 'email'){
+            if (value===''){
+                setErrorMessage('Email is required')
+            } else if (!/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,3})+$/.test(value)){
+                setErrorMessage('You must enter a valid email')
 
+            }
+        }
+        if (name === 'message'){
+            if (value===''){
+                setErrorMessage('Message is required')
+            }
+        }
+    }
     const handleSubmit = (e) => {
         e.preventDefault();
+        if (!/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,3})+$/.test(email)){
+            setErrorMessage('Invalid Email')
+            return
+        }
+        console.log(name,email,message)
     }
 
     return (
@@ -33,8 +59,10 @@ function Contact () {
                         name='name'
                         type='text'
                         value={name}
-                        onBlur={handleInput}
+                        onBlur={checkInput}
                         onChange={handleInput}
+                        onClick={()=>setErrorMessage('')}
+
                         placeholder='Name'
                         className="contactInput contactCard"
                         required
@@ -50,7 +78,8 @@ function Contact () {
                     type='email'
                     value={email}
                     onChange={handleInput}
-
+                    onBlur={checkInput}
+                    onClick={()=>setErrorMessage('')}
                     placeholder='Email'
                     className="contactInput contactCard"
 
@@ -62,9 +91,13 @@ function Contact () {
                 <textarea
                     name="message"
                     value={message}
+                    onBlur={checkInput}
+                    onClick={()=>setErrorMessage('')}
+
                     onChange={handleInput}
                     className="contactMessage contactCard"
                 ></textarea>
+                <div>{errorMessage}</div>
                 <div className="contactBtnContainer">
 
                 <button className="contactBtn">SUBMIT</button>
