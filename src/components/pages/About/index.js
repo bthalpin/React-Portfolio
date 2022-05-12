045e3145303f0c0fb82bfd5db677aab625342cdc
bthalpin <brian.t.halpin@gmail.com>
2022-05-12 13:15:20 -0400

    Change wording for about me section

--- src/components/pages/About/index.js
+++ src/components/pages/About/index.js
@@ -22,6 +22,22 @@ function About () {
                         has given me an avenue to not only do what I love, but also to engage me and 
                         challenge me to always improve my skills.
                     </p>    
+                    <p id="statement">
+                        I earned a certificate in Full Stack Web Development from Rutgers University.  
+                        Before starting the program I took it upon myself to learn as much as I could, 
+                        even developing a real time chat application using React, Node, Express, Knex, 
+                        Postgres, and socket.io.  In my time working through the program, I was able to 
+                        hone my skills as a React developer, learn the importance of folder structure, 
+                        and learn new technologies like GraphQL.  One of my most important experiences 
+                        was collaborating with other developers throughout the program, and in some 
+                        instances, using my many years of supervisory experience to help lead the team 
+                        to complete the project effectively and efficiently.  
+                    </p>    
+                    <p id="statement">
+                        My love of problem solving, my drive to always improve, and my strong collaborative 
+                        skills make me an advantageous candidate for any team.
+
+                    </p>    
 
                 </section>
         </div>
